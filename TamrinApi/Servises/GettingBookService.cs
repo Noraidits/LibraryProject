20324ae92dca71da88cec70bfd668b12bd5fd7b4
Your Name <your.email@example.com>
2025-09-14 14:37:01 +0330

    GettingBooksevice add 4 if, and removecopy , add active book

--- TamrinApi/Servises/GettingBookService.cs
+++ TamrinApi/Servises/GettingBookService.cs
@@ -15,9 +15,11 @@ namespace TamrinApi.Servises
         {
             if (_member.GetMemberById(memberId) == null) throw new Exception("your member is not exist");
             if (_book.getBookById(bookid) == null) throw new Exception("your book is not exist");
-
-
-
+            if (_member.memberCanBorrow(bookid)) throw new Exception("you can't borrow book(expiring or full 5 book");
+            if (_book.IsbookExistForGet(bookid)) throw new Exception("your target book is not in library");
+            
+            _book.removeCopy(bookid,1);
+            _member.addActiveBook(memberId);
         }
     }
 }
