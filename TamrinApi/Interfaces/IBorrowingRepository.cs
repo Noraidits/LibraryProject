479303db92cc05f87e8d476f53c8f8b88d14b505
Your Name <your.email@example.com>
2025-09-16 16:20:18 +0330

    Add GetAllbrrowing in barrow(repo & interface) And fix updatereturndate

--- TamrinApi/Interfaces/IBorrowingRepository.cs
+++ TamrinApi/Interfaces/IBorrowingRepository.cs
@@ -6,10 +6,11 @@ namespace TamrinApi.Interfaces
     public interface IBorrowingRepository
     {
         Borrowing? GetBorrowingByid(Guid id);
+        IEnumerable<Borrowing> GetAllborrowing();
         IEnumerable<Borrowing> GetBorrowByMember(Guid memberid);
         IEnumerable<Borrowing> GetBorrowByBook(Guid bookid);
         void addBorrow(Borrowing borrow);
-        void updatereturndate (DateOnly returndaate, Guid id);
+        void updatereturndate (Guid id);
         void AddborrowingForservice(Guid memberId, Guid bookId);
     }
 
